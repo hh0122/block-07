ac79069eac68a7acae675ade1d27aed8925f0f0d
Henry Huynh <huynh.vu2201@gmail.com>
2024-10-09 11:17:16 -0500

    initial commit

---
+++ vault.js
@@ -0,0 +1,8 @@
+const secretMessage =` You have received this message because you have been chosen to open an important vault. Here is the secret combination:`
+
+
+const code1 = 30 / 3; //10
+const code2 = code1 * 4; // 40
+const code3 = code2 - 1; // 39
+
+alert(`${secretMessage} ${code1} - ${code2} - ${code3}`);
