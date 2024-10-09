ff1b1798e9de6707e9960e4b3dc947916f60324f
Henry Huynh <huynh.vu2201@gmail.com>
2024-10-09 11:20:26 -0500

    add more comment

--- vault.js
+++ vault.js
@@ -1,6 +1,7 @@
+// Show secret message
 const secretMessage =` You have received this message because you have been chosen to open an important vault. Here is the secret combination:`
 
-
+// perform calculation
 const code1 = 30 / 3; //10
 const code2 = code1 * 4; // 40
 const code3 = code2 - 1; // 39
